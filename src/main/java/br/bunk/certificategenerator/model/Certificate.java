711b26fd121fb8a98ba1d3406b5cf8b4d1dfab67
Evictory <emerson_vitorio@live.com>
2021-01-10 21:05:21 -0300

    Add jsoup dependency to manipulate HTML as well one function to do that

--- src/main/java/br/bunk/certificategenerator/model/Certificate.java
+++ src/main/java/br/bunk/certificategenerator/model/Certificate.java
@@ -3,11 +3,11 @@ import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 public class Certificate {
     private static final String HTML = "src/main/resources/templates/certificate.html";
@@ -15,10 +15,23 @@ public class Certificate {
     //remove, only tests proposes
     public static void main(String[] args) {
         try {
+            transformHTMLPDF();
             generateCertificatePDF(HTML);
         } catch (IOException | ParserConfigurationException | DocumentException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void transformHTMLPDF() throws IOException {
+        File input = new File(HTML);
+        org.jsoup.nodes.Document doc = Jsoup.parse(input, "UTF-8", HTML);
+        Element div = doc.select("div#title").first();
+        div.html("<p>Alterado</p>");
+
+        FileWriter writer = new FileWriter(HTML);
+        writer.write(doc.toString());
+        writer.flush();
+        writer.close();
     }
 
     private static void generateCertificatePDF(String filename) throws ParserConfigurationException, IOException, DocumentException {
